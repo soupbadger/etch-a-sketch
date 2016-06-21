3d0dde52d2b42eadf59d4d9da2cbc40f84d1c976
soupbadger <soupbadger@gmail.com>
2016-06-20 20:47:02 -0400

    first commit
    Odd bug when switching from default to RNG. Look into it later.

---
+++ sketh.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  var $squares;
+  var mode = "default";
+
+  function sketch(size) {
+    var $sketchpad = $("#sketchpad");
+    var pHeight = $($sketchpad).height();
+    var pWidth = $($sketchpad).width();
+
+    $sketchpad.html("");
+
+    var pRow = "";
+
+    for (var i = 0; i < size; i++) {
+      pRow += "<div class='square'></div>";
+    };
+
+    for (var i = 0; i < size; i++) {
+      $($sketchpad).append(pRow);
+    };
+    
+    $squares = $("div .square");
+		var squareH = (pHeight / size) + "px";
+		var squareW = (pWidth / size) + "px";
+		$($squares).css("height", squareH);
+		$($squares).css("width", squareW);
+		draw();
+  };
+  
+  sketch(16);
+  
+  function draw() {
+    if (mode == "default") {
+      $($squares).mouseenter(function() {
+				$(this).css("background-color", "darkgray");
+			});
+		}	else if(mode == "RNG") {
+			$($squares).mouseenter(function() {
+				$(this).css("background-color", getColor());
+			});
+		};
+	};
+  
+  $("#clear:button").click(function(){
+    do {
+      var newSize = prompt("Enter new pad size: ");
+      sketch(newSize);
+    } while (isNaN(newSize));
+  });
+    
+  $("#default:button").click(function() {
+    mode = "default";
+    do {
+      var newSize = prompt("Enter new pad size: ");
+      sketch(newSize);
+    } while (isNaN(newSize));
+  });
+  
+  $("#RNG:button").click(function() {
+		mode = "RNG";
+		do {
+			var newPadSize = prompt("Please enter a new size.");
+			newPad(newPadSize);
+		}
+		while (isNaN(newPadSize));
+	});
+  
+  function getColor() {
+    	var digits = '0123456789ABCDEF'.split('');
+    	var color = '#';
+    	for (var i = 0; i < 6; i++ ) {
+        	color += digits[Math.floor(Math.random() * 16)];
+    	}
+    	return color;
+	};
+  
+  
+})
